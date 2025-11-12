253ea1f510b0ff57b041868a9a5851e3826bc648
Ubuntu <ubuntu@ip-172-31-3-43.ap-south-1.compute.internal>
2025-11-12 15:30:59 +0000

    project

--- app.js
+++ app.js
@@ -1,88 +1,102 @@
 const express = require('express'),
-    bodyParser = require('body-parser'),
-    // In order to use PUT HTTP verb to edit item
-    methodOverride = require('method-override'),
-    // Mitigate XSS using sanitizer
-    sanitizer = require('sanitizer'),
-    app = express(),
-    port = 8000
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  sanitizer = require('sanitizer'),
+  app = express(),
+  port = 8000;
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-// https: //github.com/expressjs/method-override#custom-logic
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Enable PUT/DELETE overrides
 app.use(methodOverride(function (req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        // look in urlencoded POST bodies and delete it
-        let method = req.body._method;
-        delete req.body._method;
-        return method
-    }
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    let method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
 }));
-
 
 let todolist = [];
 
-/* The to do list and the form are displayed */
+/* Display To-Do list */
 app.get('/todo', function (req, res) {
-        res.render('todo.ejs', {
-            todolist,
-            clickHandler: "func1();"
-        });
-    })
+  res.render('todo.ejs', {
+    todolist,
+    clickHandler: "func1();"
+  });
+});
 
-    /* Adding an item to the to do list */
-    .post('/todo/add/', function (req, res) {
-        // Escapes HTML special characters in attribute values as HTML entities
-        let newTodo = sanitizer.escape(req.body.newtodo);
-        if (req.body.newtodo != '') {
-            todolist.push(newTodo);
-        }
-        res.redirect('/todo');
-    })
+/* Add new task */
+app.post('/todo/add', function (req, res) {
+  let newTodo = sanitizer.escape(req.body.newtodo);
+  let priority = req.body.priority || 'low';
 
-    /* Deletes an item from the to do list */
-    .get('/todo/delete/:id', function (req, res) {
-        if (req.params.id != '') {
-            todolist.splice(req.params.id, 1);
-        }
-        res.redirect('/todo');
-    })
-
-    // Get a single todo item and render edit page
-    .get('/todo/:id', function (req, res) {
-        let todoIdx = req.params.id;
-        let todo = todolist[todoIdx];
-
-        if (todo) {
-            res.render('edititem.ejs', {
-                todoIdx,
-                todo,
-                clickHandler: "func1();"
-            });
-        } else {
-            res.redirect('/todo');
-        }
-    })
-
-    // Edit item in the todo list 
-    .put('/todo/edit/:id', function (req, res) {
-        let todoIdx = req.params.id;
-        // Escapes HTML special characters in attribute values as HTML entities
-        let editTodo = sanitizer.escape(req.body.editTodo);
-        if (todoIdx != '' && editTodo != '') {
-            todolist[todoIdx] = editTodo;
-        }
-        res.redirect('/todo');
-    })
-    /* Redirects to the to do list if the page requested is not found */
-    .use(function (req, res, next) {
-        res.redirect('/todo');
-    })
-
-    .listen(port, function () {
-        // Logging to console
-        console.log(`Todolist running on http://0.0.0.0:${port}`)
+  if (newTodo && newTodo.trim() !== '') {
+    todolist.push({
+      text: newTodo.trim(),
+      completed: false,
+      priority
     });
-// Export app
+  }
+  res.redirect('/todo');
+});
+
+/* Delete task */
+app.get('/todo/delete/:id', function (req, res) {
+  if (req.params.id !== '') {
+    todolist.splice(req.params.id, 1);
+  }
+  res.redirect('/todo');
+});
+
+/* Toggle complete */
+app.get('/todo/complete/:id', function (req, res) {
+  const todo = todolist[req.params.id];
+  if (todo) {
+    todo.completed = !todo.completed;
+  }
+  res.redirect('/todo');
+});
+
+/* Clear all completed tasks */
+app.post('/todo/clearCompleted', function (req, res) {
+  todolist = todolist.filter(t => !t.completed);
+  res.redirect('/todo');
+});
+
+/* Edit task (GET form) */
+app.get('/todo/edit/:id', function (req, res) {
+  let todoIdx = req.params.id;
+  let todo = todolist[todoIdx];
+
+  if (todo) {
+    res.render('edititem.ejs', {
+      todoIdx,
+      todo: todo.text,
+      clickHandler: "func1();"
+    });
+  } else {
+    res.redirect('/todo');
+  }
+});
+
+/* Update task (PUT) */
+app.put('/todo/edit/:id', function (req, res) {
+  let todoIdx = req.params.id;
+  let editTodo = sanitizer.escape(req.body.editTodo);
+
+  if (todoIdx !== '' && editTodo !== '') {
+    todolist[todoIdx].text = editTodo.trim();
+  }
+  res.redirect('/todo');
+});
+
+/* Default redirect */
+app.use((req, res) => res.redirect('/todo'));
+
+app.listen(port, function () {
+  console.log(`✅ To-Do List running on http://0.0.0.0:${port}`);
+});
+
 module.exports = app;
+
